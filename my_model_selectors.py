d8eb5c7d373529d263709fc0e37887d869621d19
Jon Katzur <jkatzur@gmail.com>
2017-10-09 00:26:01 -0400

    Finished BIC - refactored best_model for cleanliness

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -33,26 +33,6 @@ class ModelSelector(object):
 
     # Created this function within the ModelSelector class so that I could repeat
     # methodology between SelectorCV, BIC, and DIC methods without repeating code
-    def best_model(self, values, state_range, is_max):
-        """
-            Given an array of values representing model score, return the model with the
-            number of components that optimize the model score
-        """
-        # This section determines which number of components has the highest average log Likelihood
-        best_num_components = self.min_n_components
-        is_best = values[self.min_n_components]
-        if is_max:
-            for i in state_range:
-                if values[i] > is_best:
-                    is_best = values[i]
-                    best_num_components = i
-        else:
-            for i in state_range:
-                if values[i] < is_best:
-                    is_best = values[i]
-                    best_num_components = i
-        # Return the best model
-        return self.base_model(best_num_components)
 
     def base_model(self, num_states):
         # with warnings.catch_warnings():
@@ -69,6 +49,27 @@ class ModelSelector(object):
                 print("failure on {} with {} states".format(self.this_word, num_states))
             return None
 
+
+def best_model(values, min_states, max_states, is_max):
+    """
+        Given an array of values representing model score, return the model with the
+        number of components that optimize the model score
+    """
+    # This section determines which number of components has the highest average log Likelihood
+    best_num_components = min_states
+    is_best = values[min_states]
+    if is_max:
+        for i in range(min_states, max_states):
+            if values[i] > is_best:
+                is_best = values[i]
+                best_num_components = i
+    else:
+        for i in range(min_states, max_states):
+            if values[i] < is_best:
+                is_best = values[i]
+                best_num_components = i
+    # Return the best model
+    return best_num_components
 
 class SelectorConstant(ModelSelector):
     """ select the model with value self.n_constant
@@ -129,7 +130,7 @@ class SelectorBIC(ModelSelector):
             pass
 
         # Moved the picking of best model into a function in the ModelSelector class for consistency between Model types
-        return self.best_model(values, state_range, is_max=False)
+        return self.base_model(best_model(values, self.min_n_components, self.max_n_components+1, is_max=True))
 
 
 class SelectorDIC(ModelSelector):
@@ -140,14 +141,51 @@ class SelectorDIC(ModelSelector):
     http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
     https://pdfs.semanticscholar.org/ed3d/7c4a5f607201f3848d4c02dd9ba17c791fc2.pdf
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
+
+
+    The philosophical idea of DIC is that we should look at how much more likely a model is to
+    correctly identify a particular word than other words in the vocabulary. Essentially it looks at
+    the incremental likelihood of the model correctly identifying the right word over the average scores
+    for other words in the vocab.
+
+    To calculate this we use two terms ->
+        1) The Log Likelihood of the word being picked, and
+        2) Average Log Likelihood of every other word in the vocab using the model
+
+    We then take the difference of terms to calculate the incremental
+
     '''
 
+    def comparison_ll(self, model):
+        scores = []
+        # print("In Comparison Function")
+        for word, (testX, testLengths) in self.hwords.items():
+            scores.append(model.score(testX, testLengths))
+        return scores
+
     def select(self):
-        warnings.filterwarnings("ignore", category=DeprecationWarning)
+        try:
+            state_range = range(self.min_n_components, self.max_n_components+1)
+            # This is how I track the BIC for each num components
+            values = {i:0 for i in state_range}
+            models = {i:0 for i in state_range}
+            # For each potential number of components do the following...
+            for i in state_range:
+                # This was tricky to me at first. I need to fit the model on this_word first
+                # using the self information and then pass that model to be scored with all of the
+                # comparison words. This helped -> https://discussions.udacity.com/t/selector-dic-taking-a-long-time-to-run/334060
+                models[i] = self.base_model(i)
+                # log Likelihood
+                L_word = models[i].score(self.X,self.lengths)
+                values[i] = L_word - np.mean(self.comparison_ll(models[i]))
+                # print("For I = {} states; LL = {}, np.mean = {},DIC = {}".format(i, L_word, np.mean(self.comparison_ll(models[i])), values[i]))
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        # Error handle in case model does not return a potential match
+        except:
+            pass
 
+        # Moved the picking of best model into a function in the ModelSelector class for consistency between Model types
+        return self.base_model(best_model(values, self.min_n_components, self.max_n_components+1, is_max=True))
 
 class SelectorCV(ModelSelector):
     ''' select best model based on average log Likelihood of cross-validation folds
@@ -188,4 +226,4 @@ class SelectorCV(ModelSelector):
             pass
 
         # Moved the picking of best model into a function in the ModelSelector class for consistency between Model types
-        return self.best_model(values, state_range, is_max=True)
+        return self.base_model(best_model(values, self.min_n_components, self.max_n_components+1, is_max=True))
