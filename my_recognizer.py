742e6b6aa7871a4447566d7cf32d779b123df949
Jon Katzur <jkatzur@gmail.com>
2017-10-09 01:03:03 -0400

    Improved documentation

--- my_recognizer.py
+++ my_recognizer.py
@@ -16,20 +16,26 @@ def recognize(models: dict, test_set: SinglesData):
             ]
        guesses is a list of the best guess words ordered by the test set word_id
            ['WORDGUESS0', 'WORDGUESS1', 'WORDGUESS2',...]
+
+    Similar to DIC model - the model is trained elsewhere and this is just running it on test data
+    The structure here is to run through each test word and compare the results for each of the word-models
+    Then, we identify which word-model scores the best, and make our guess that it's that word
     """
     probabilities = []
     guesses = []
+    # Copied this structure from my DIC implementation - it's a similar sort've paradigm
     for word, (testX, testLengths) in test_set.get_all_Xlengths().items():
         is_best = float("-inf")
         prob_dict = {}
         for (trained, model) in models.items():
-            # Set this up due to -> https://discussions.udacity.com/t/failure-in-recognizer-unit-tests/240082/5?u=cleyton_messias
+            # This was tricky. I only figured it out through the forum here -> https://discussions.udacity.com/t/failure-in-recognizer-unit-tests/240082/5?u=cleyton_messias
             try:
                 test_prof = model.score(testX,testLengths)
                 prob_dict[trained] = test_prof
             except:
                 prob_dict[trained] = float("-inf")
-            if test_prof > is_best:
+            # this structure is just a normal pick the max value
+            if test_prof > is_bests:
                 is_best = test_prof
                 best_word = trained
         probabilities.append(prob_dict)
